cac3593ee017fc6933491f3dff950698635da11b
Allen Rohner <arohner@gmail.com>
2015-03-20 12:17:54 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datomic-compound-index "0.1.0"
+(defproject datomic-compound-index "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
