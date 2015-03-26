87dab748b536f578a09c40d47c303d426689e25b
Allen Rohner <arohner@gmail.com>
2015-03-26 11:59:43 -0500

    WIP entities

--- src/datomic_compound_index/core.clj
+++ src/datomic_compound_index/core.clj
@@ -6,75 +6,79 @@
            datomic.db.Datum
            java.util.regex.Pattern))
 
-;; approaches
-;; get encoding to work properly
 
-(defprotocol SerializeKey
-  (to-bytes [this])
-  (from-bytes [this]))
+(defn attribute
+  "Install a new schema attribute"
+  [ident type & {:as opts}]
+  (merge {:db/id (d/tempid :db.part/db)
+          :db/ident ident
+          :db/valueType type
+          :db.install/_attribute :db.part/db}
+         {:db/cardinality :db.cardinality/one}
+         opts))
 
-(extend-protocol SerializeKey
-  java.lang.Long
-  (to-bytes [x]
-    (* -1 x))
-  (from-bytes [x]
-    (* -1 x)))
+(defn install-dci
+  "Returns a set of datums to define attributes. Must d/transact results before attempting to install a compound index"
+  []
+  [(attribute :dci/refs :db.type/ref
+              :db/cardinality :db.cardinality/many)
+   (attribute :dci/index-name :db.type/keyword
+              :db/index true)
+   (attribute :dci/segment-attr :db.type/keyword)
+   (attribute :dci/segment-index :db.type/long)
 
-(defn serialize-key []
-  ()
+   (attribute :dci/entity :db.type/ref)
+   ;;;...
+   ])
+
+(defn compound-index
+  "Defines a new compound index. Returns a set of datums that insert attributes. d/transact results"
+  [index-name segments]
+  (concat
+
+   (concat (map-indexed (fn [i s]
+                          {:db/id (d/tempid :db.part/user)
+                           :dci/index-name index-name
+                           :dci/segment-attr s
+                           :dci/segment-index i}) segments))))
+
+(defn get-index-attrs
+  "Given the name of a compound index, returns a vector of attr names that comprise the index"
+  [db index]
+  (->> (d/q '[:find [(pull ?e [:dci/segment-attr :dci/segment-index]) ...] :in $ ?index :where [?e :dci/index-name ?index]] db index)
+       (sort-by :dci/segment-index)
+       (map :dci/segment-attr)))
+
+(def value-type-map {:db.type/instant :dci/value-instant
+                     :db.type/keyword :dci/value-keyword
+                     :db.type/long :dci/value-long
+                     :db.type/ref :dci/value-ref
+                     :db.type/string :dci/value-string})
+
+(defn index-value
+  "Returns a set of datums to properly index entity e. e should be a d/entity, or a map that contains all values in the index"
+  [db e index]
+  (let [attrs (get-index-attrs db index)]
+    (assert (seq attrs))
+    (let [state (reduce (fn [{:keys [last-val datums] :as state} attr]
+                          (update-in state [:datums] conj (merge
+                                                           {:db/id (d/tempid :db.part/user)
+                                                            :dci/entity (:db/id e)}
+                                                           (when last-val
+                                                             {:dci/refs last-val})))) {:last-val nil
+                                                                                       :datums []} (reverse attrs))
+          datums (:datums state)
+          first-segment (last datums)
+          index-entity (d/q '[:find ?e . :in $ ?index :where [?e :dci/index-name ?index]] db index)]
+      (assert index-entity)
+      (conj datums {:db/id index-entity
+                    :dci/refs first-segment}))))
+
+(defn search* [db entities attrs vals]
+  (mapcat (d/q '[:find ?e :in $ :where ]))
   )
-
-(defn deserialize-key [])
-(defn index-key
-  "Create an index key. Can be used for inserting, or querying.
-
-  key is a vector of datomic values"
-  [key]
-  (.array (fress/write (to-bytes key))))
-
-(defn compound-compare
-  "compare operation. If the key is shorter than value, and all previous sub-keys match, returns 0"
-  [ak bk]
-  (loop [[a & as] ak
-         [b & bs] bk]
-    (if (or (nil? a)
-            (nil? b))
-      0
-      (let [result (compare a b)]
-        (if (= 0 result)
-          (recur as bs)
-          result)))))
-
-(defn search
-  "Search a compound index. attr is the attribute to search. Returns a seq of datoms. key, key1,key2 are vectors that will be passed to index-key.
-
-   Passing a single key returns all datoms , two keys uses d/index-range.
-
-   (search db :event/user-at-type [1234 (Date.) {:partial? true}])
- "
-  ([db attr key]
-   (let [attr-id (:id (d/attribute db attr))]
-     (->> (d/seek-datoms db :avet attr key)
-          (mapv identity)
-          (seq)
-          (drop-while (fn [^Datum d]
-                        (and (= attr-id (.a d))
-                             (< (compound-compare (fress/read (.v d)) key) 0))))
-          (take-while (fn [^Datum d]
-                        (and (= attr-id (.a d))
-                             (= (compound-compare (fress/read (.v d)) key) 0))))))))
-
-(defn search-range
-  "Search a compound index. Returns a seq of datoms.
-
-  Uses d/index-range to find all datoms between key1 and key2, inclusive"
-  [db attr key1 key2]
-  (let [attr-id (:id (d/attribute db attr))]
-    (->> (d/seek-datoms db :avet attr key1)
-         (seq)
-         (drop-while (fn [^Datum d]
-                       (and (= attr-id (.a d))
-                            (< (compound-compare (fress/read (.v d)) key1) 0))))
-         (take-while (fn [^Datum d]
-                       (and (= attr-id (.a d))
-                            (<= (compound-compare (fress/read (.v d)) key2) 0)))))))
+(defn search [db index vals]
+  (let [a (get-index-attrs index)
+        v (first vals)]
+    (d/q '[:find ?e :in $ :where [?e ?a ?v]] db a v))
+  )
--- test/datomic_compound_index/core_test.clj
+++ test/datomic_compound_index/core_test.clj
@@ -10,237 +10,36 @@
   (:import java.util.Date
            datomic.db.Datum))
 
-(defn attribute
-  "Install a new schema attribute"
-  [ident type & {:as opts}]
-  (merge {:db/id (d/tempid :db.part/db)
-          :db/ident ident
-          :db/valueType type
-          :db.install/_attribute :db.part/db}
-         {:db/cardinality :db.cardinality/one}
-         opts))
-
 (defn empty-database []
   (let [db-url (str "datomic:mem://" (gensym "db"))]
     (d/create-database db-url)
     (d/connect db-url)))
 
-(deftest index-key-uses-to-datomic-for-dates
-  (let [v (index-key [(Date.) 1234])]
-    (is (string? v))
-    (is (re-find #"^[0-9|]+$" v))))
+(def event-schema (concat
+                   [(attribute :user/name :db.type/string)
+                    (attribute :event/user :db.type/ref)
+                    (attribute :event/at :db.type/instant)
+                    (attribute :event/type :db.type/keyword)]
+                   (compound-index :event/user-at-type [:event/user :event/at :event/type])))
 
-(deftest index-key-works
-  (is (index-key [1 :foo])))
-
-(deftest search-finds-things-correctly
-  (let [schema [(attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]
-        conn (empty-database)]
-    @(d/transact conn schema)
-    @(d/transact conn [{:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [1 "foo"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [1 "bar"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [11 "bbq"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [2 "baz"])}])
-    (is (= 2 (count (search (d/db conn) :user/foo-bar [1]))))
-    (is (= 1 (count (search (d/db conn) :user/foo-bar [1 "bar"]))))
-    (is (= 0 (count (search (d/db conn) :user/foo-bar [1 "bogus"]))))))
-
-(deftest search-range-finds-things-correctly
-  (let [schema [(attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]
-        conn (empty-database)]
-    @(d/transact conn schema)
-    @(d/transact conn [{:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [1 "foo"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [2 "bar"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [3 "bbq"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [4 "baz"])}
-                       {:db/id (d/tempid :db.part/user)
-                        :user/foo-bar (index-key [5 "quux"])}])
-    (let [result (search-range (d/db conn) :user/foo-bar [2] [4])]
-      (is (= [[2 "bar"] [3 "bbq"] [4 "baz"]] (map (fn [^Datum d] (fress/read (.v d))) result))))
-
-    (let [result (search-range (d/db conn) :user/foo-bar [2 "bar"] [4 "baz"])]
-      (is (= [[2 "bar"] [3 "bbq"] [4 "baz"]] (map (fn [^Datum d] (fress/read (.v d))) result))))))
-
-
-(deftest compound-compare-works
-  (are [a b op v] (op (compound-compare a b) v)
-       [0] [0] = 0
-       [0] [1] < 0
-       [-4] [-2] < 0
-       [-4] [2] < 0
-       [0 0] [1 1] < 0
-       [-1 0] [0 0] < 0))
-
-(defn normalize-compare
-  "Returns -1, 0,1 to make testing easier"
-  [c]
-  (cond
-    (> c 0) 1
-    (< c 0) -1
-    :else 0))
-
-(defspec index-key-preserves-order
-  (prop/for-all [[a b] (gen/tuple gen/int gen/int)]
-    (= (normalize-compare (compare a b)) (normalize-compare (compound-compare (index-key a) (index-key b))))))
-
-(defn search-vector [db eid]
-  ((juxt :user/foo :user/bar) (d/pull db [:user/foo :user/bar] eid)))
-
-;; search-spec TODO
-;; - make query separate from values
-(defspec search-spec
-  100
+(deftest create-get-index-works
   (let [conn (empty-database)
-        schema [(attribute :user/foo :db.type/long)
-                (attribute :user/bar :db.type/long)
-                (attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]]
-    @(d/transact conn schema)
-    (prop/for-all [values (gen/vector (gen/tuple gen/pos-int gen/pos-int))]
-      @(d/transact conn (for [[foo bar] values]
-                          {:db/id (d/tempid :db.part/user)
-                           :user/foo foo
-                           :user/bar bar
-                           :user/foo-bar (index-key [foo bar])}))
-      (let [db (d/db conn)]
-        (every? (fn [[foo bar]]
-                  (let [datomic (->> (d/q '[:find [?e ...] :in $ ?foo ?bar :where
-                                            [?e :user/foo ?foo]
-                                            [?e :user/bar ?bar]] db foo bar)
-                                     (sort))
-                        search-result (->> (search db :user/foo-bar [foo bar])
-                                           (mapv (fn [^Datum datom]
-                                                   (.e datom)))
-                                           (sort))
-                        eql? (= datomic search-result)]
-                    (when-not eql?
-                      (let [all-values (d/q '[:find [?e ...] :in $ :where [?e :user/foo]] (d/db conn))
-                            datomic (->>
-                                     (d/q '[:find [?e ...] :in $ ?foo ?bar :where
-                                            [?e :user/foo ?foo]
-                                            [?e :user/bar ?bar]] db foo bar)
-                                     (mapv (fn [eid]
-                                             (search-vector db eid))))
-                            search-result (->>
-                                           (search db :user/foo-bar [foo bar])
-                                           (mapv (fn [datom]
-                                                   (search-vector db (.e datom)))))]
-                        (inspect values)
-                        (inspect [foo bar])
-                        (inspect datomic)
-                        (inspect search-result)))
-                    eql?)) values)))))
+        schema event-schema]
+    @(d/transact conn (install-dci))
+    @(d/transact conn event-schema)
+    (is (= [:event/user :event/at :event/type] (get-index-attrs (d/db conn) :event/user-at-type)))))
 
-(defn search-test
-  "Values is a seq of [foo bar] pairs. Query is a vector [foo bar] result is a set of vectors [foo bar]"
-  [values query expected]
+(deftest index-value-works
   (let [conn (empty-database)
-        schema [(attribute :user/foo :db.type/long)
-                (attribute :user/bar :db.type/long)
-                (attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]]
-    @(d/transact conn schema)
-    @(d/transact conn (for [[foo bar] values]
-                        {:db/id (d/tempid :db.part/user)
-                         :user/foo foo
-                         :user/bar bar
-                         :user/foo-bar (index-key [foo bar])}))
-    (let [datoms (search (d/db conn) :user/foo-bar query)
-          result (set (map (fn [d]
-                             ((juxt :user/foo :user/bar) (d/pull (d/db conn) [:user/foo :user/bar] (.e d)))) datoms))]
-      (is (= expected result)))))
-
-(deftest search-tests
-  ;; (search-test [] [] #{})
-  ;; (search-test [[0 0]] [0 0] #{[0 0]})
-  ;; (search-test [[1 -1] [1 0]] [1 0] #{[1 0]})
-
-  (search-test [[2 4] [-4 -2] [2 -4]] [-4 -2] #{[-4 -2]})
-  )
-
-(defspec search-range-spec
-  100
-  (let [conn (empty-database)
-        schema [(attribute :user/foo :db.type/long)
-                (attribute :user/bar :db.type/long)
-                (attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]]
-    @(d/transact conn schema)
-    (prop/for-all [values (gen/vector (gen/tuple gen/pos-int gen/pos-int))
-                   query (gen/such-that (fn [[[f1 b1] [f2 b2]]]
-                                          (< f1 f2)) (gen/tuple (gen/tuple gen/pos-int gen/pos-int) (gen/tuple gen/pos-int gen/pos-int)))]
-      @(d/transact conn (for [[foo bar] values]
-                          {:db/id (d/tempid :db.part/user)
-                           :user/foo foo
-                           :user/bar bar
-                           :user/foo-bar (index-key [foo bar])}))
-      (let [db (d/db conn)]
-        (let [[[foo1 bar1] [foo2 bar2]] query
-              datomic (->> (d/q '[:find [?e ...] :in $ ?foo1 ?foo2 ?bar1 ?bar2 :where
-                                  [?e :user/foo ?efoo]
-                                  [?e :user/bar ?ebar]
-                                  [(<= ?foo1 ?efoo)]
-                                  [(<= ?efoo ?foo2)]
-                                  [(<= ?bar1 ?ebar)]
-                                  [(<= ?ebar ?bar2)]] db foo1 foo2 bar1 bar2)
-                           (sort))
-              search-result (->> (search-range db :user/foo-bar [foo1 bar1] [foo2 bar2])
-                                 (mapv (fn [^Datum datom]
-                                         (.e datom)))
-                                 (sort))
-              eql? (= datomic search-result)]
-          (when-not eql?
-            (let [all-values (d/q '[:find [?e ...] :in $ :where [?e :user/foo]] (d/db conn))
-                  all-values (mapv #(search-vector (d/db conn) %) all-values)
-                  datomic-result (vec (map #(search-vector (d/db conn) %) datomic))
-                  search-result (vec (map #(search-vector (d/db conn) %) search-result))]
-              (inspect all-values)
-              (inspect query)
-              (inspect datomic-result)
-              (inspect search-result)
-              ;; (inspect datomic)
-              ;; (inspect search-result)
-              ))
-          eql?)))))
-
-(defn search-range-test
-  "Values is a seq of [foo bar] pairs. Query is a vector [[foo bar] [foo bar]] result is a set of vectors [foo bar]"
-  [values query expected]
-  (let [conn (empty-database)
-        schema [(attribute :user/foo :db.type/long)
-                (attribute :user/bar :db.type/long)
-                (attribute :user/foo-bar :db.type/bytes
-                           :db/index true)]]
-    @(d/transact conn schema)
-    @(d/transact conn (for [[foo bar] values]
-                        {:db/id (d/tempid :db.part/user)
-                         :user/foo foo
-                         :user/bar bar
-                         :user/foo-bar (index-key [foo bar])}))
-    (let [datoms (apply search-range (d/db conn) :user/foo-bar query)
-          result (set (map (fn [d]
-                             ((juxt :user/foo :user/bar) (d/pull (d/db conn) [:user/foo :user/bar] (.e d)))) datoms))]
-      (is (= expected result)))))
-
-(deftest search-range-1
-  (search-range-test [[0 1]] [[0 0] [0 2]] #{[0 1]})
-  (search-range-test [[0 0]] [[0 1] [0 2]] #{})
-  (search-range-test [[-1 1]] [[2 0] [3 0]] #{})
-  (search-range-test [[0 0]] [[0 0] [0 0]] #{[0 0]})
-  (search-range-test [[0 0] [1 1] [2 2]] [[3 3] [4 4]] #{})
-  (search-range-test [[0 0] [1 1] [2 2]] [[-2 -2] [-3 -3]] #{})
-  (search-range-test [[0 0] [1 1] [2 2]] [[-1 -2] [-3 -3]] #{})
-  (search-range-test [[-2 0] [1 2]] [[-4 0] [4 1]] #{[-2 0] [1 2]}))
-
-;; (defspec search-range-partial)
-;; (defspec search-partial)
+        schema event-schema]
+    @(d/transact conn (install-dci))
+    @(d/transact conn event-schema)
+    (let [user {:db/id (d/tempid :db.part/user)
+                :user/name "allen"}
+          event {:db/id (d/tempid :db.part/user)
+                 :event/user (:db/id user)
+                 :event/at (Date.)
+                 :event/type :foo}
+          index-val (index-value (d/db conn) event :event/user-at-type)]
+      (inspect index-val)
+      @(d/transact conn (concat [user event] index-val)))))
