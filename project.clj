61ceb62e12fafe16bb658696d6152094b4dbf233
Allen Rohner <arohner@gmail.com>
2015-03-31 14:03:08 -0500

    Move -Xmx to lein

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5130"]
                                   [org.clojure/data.fressian "0.2.0"]
-                                  [org.clojure/test.check "0.5.9"]]}})
+                                  [org.clojure/test.check "0.5.9"]]}
+             :test {:jvm_opts ["-Xmx2048m"]}})
