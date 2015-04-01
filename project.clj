0ef261b84b99af3555e1dfa940bf3c3833509b98
Allen Rohner <arohner@gmail.com>
2015-04-01 15:57:41 -0500

    Version 0.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datomic-compound-index "0.2.3"
+(defproject datomic-compound-index "0.2.4-SNAPSHOT"
   :description "A small library for adding compound indices to Datomic"
   :url "https://github.com/arohner/datomic-compound-index"
   :license {:name "Eclipse Public License"
