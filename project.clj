b177250378004fa6af28d8a217f46c4713c3df66
Allen Rohner <arohner@gmail.com>
2015-04-01 15:02:15 -0500

    Fressian is a runtime dep, not just dev-time

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject datomic-compound-index "0.2.0"
+(defproject datomic-compound-index "0.2.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/data.fressian "0.2.0"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5130"]
-                                  [org.clojure/data.fressian "0.2.0"]
                                   [org.clojure/test.check "0.5.9"]]}
              :test {:jvm-opts ["-Xmx2048m"]}}
   :global-vars  {*warn-on-reflection* true})
